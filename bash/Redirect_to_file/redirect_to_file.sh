c4eea0c5c9504cb0c3e4cbc4c6f7e1a033954fe8
AsadKhan4491 <asadmk6293@gmail.com>
2022-08-30 12:05:06 -0700

    creating a bash script folder

---
+++ bash/Redirect_to_file/redirect_to_file.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# > doesn't allow to include the previous text
+# >> allows the previous test to be concatincated'
+
+#Having a text being saved into a file
+echo "Redirecitng this text to a file" >> file.txt
+
+#Having a user enter text and then being saved into a file
+cat >> file.txt
